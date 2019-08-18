f7d93615b3c126ecdd14c3be753866270e81d1c8
Tararat Choombunchoo <tararatboom.626@gmail.com>
2019-08-18 15:47:47 +0700

    18/8/62 makeText

--- app/src/main/java/boom626/date4662/homework1/MainActivity.java
+++ app/src/main/java/boom626/date4662/homework1/MainActivity.java
@@ -1,8 +1,9 @@
 package boom626.date4662.homework1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        Toast t =  Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT);  //obj toast
+        t.show();
+
+        Toast t2 =  Toast.makeText(MainActivity.this,"Android",Toast.LENGTH_SHORT);  //obj toast
+        t2.show();
     }
 }
